39e7d74a4d3f5199618c50d91d6302700ea6dd25
Iyanuoluwa8621 <odedijipreciousjah@gmail.com>
2022-03-22 06:10:59 -0700

    my first commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,10 +4,11 @@
  * reset_to_98 - updates a integer value to 98
  *@n: A pointer to an int that will be updated
  *
- * Return: void that means it's correct
+ * Return: void that means is correct
  */
 
 void reset_to_98(int *n);
+
 {
 *n = 98;
 }
